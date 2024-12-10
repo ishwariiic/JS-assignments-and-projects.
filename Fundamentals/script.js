01dc9a53827192bcbca8c6f52b5fabe46a824bd3
Ishwari Chaudhari <148648692+ishwariiic@users.noreply.github.com>
2024-12-10 21:28:32 +0530

    Create script.js

---
+++ Fundamentals/script.js
@@ -0,0 +1,12 @@
+let country="India";
+let continent="Asia";
+let population=12747;
+let isIsland=false;
+let lang;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof lang);
